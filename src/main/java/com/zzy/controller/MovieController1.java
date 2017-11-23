d9ab62c66ef732cd5a51ac188639e40925dcd825
zhangzy <798888391@qq.com>
2017-11-23 20:04:12 +0800

    0.0.2

---
+++ src/main/java/com/zzy/controller/MovieController1.java
@@ -0,0 +1,27 @@
+package com.zzy.controller;
+
+import com.zzy.model.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Administrator on 2017/11/23.
+ */
+@RestController
+@RequestMapping("/test")
+public class MovieController1 {
+
+        @Autowired
+        private RestTemplate restTemplate;
+
+        @GetMapping(path = "/movie/{id}")
+        public UserDTO findByid(@PathVariable Long id) {
+            //通过<span style="font-family: Arial, Helvetica, sans-serif;">restTemplate访问provider服务</span>
+            return this.restTemplate.getForEntity("http://provider/simple/" + id, UserDTO.class).getBody();
+
+        }
+    }
